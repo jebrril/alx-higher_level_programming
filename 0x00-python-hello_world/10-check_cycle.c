5cfd8ae3d62ca01ec48bcb7198d1a94040557182
jebrril <125573348+jebrril@users.noreply.github.com>
2023-06-07 20:49:25 +0100

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,22 @@
-ADDC
+#include "lists.h"
+/**
+ * check_cycle - func check if the signl linked list has a check_cycle
+ * @list: list to be checked
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *thunder = list;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	for (; thunder != NULL && thunder->next != NULL;)
+	{
+		slow = slow->next;
+		thunder = thunder->next->next;
+		if (slow == thunder)
+			return (1);
+	}
+	return (0);
+}

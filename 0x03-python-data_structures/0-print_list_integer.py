10b626d4ff75de3d365ffa9a49afd898088462b2
jebrril <125573348+jebrril@users.noreply.github.com>
2023-06-11 23:38:39 +0100

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """function print all the number of list"""
-    def print_list_integer(my_list=[]):
-        for numbers in my_list:
-            if isinstance(numbers, int):
-                print('{}'.format(numbers))
 
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
+def print_list_integer(my_list=[]):
+    for numbers in my_list:
+        print('{:d}'.format(numbers))
